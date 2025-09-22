36b0fc6933a4a07fa5d1e2e94270ab5367ed828e
Jhashua10 <146078820+JhashuaC@users.noreply.github.com>
2025-09-21 19:49:43 -0600

    Improve run_all.bat pipeline script and update binary
    Enhanced the batch script to add start/end time tracking, duration calculation, and improved output formatting. The script now deletes the old Fortran executable before recompiling, updates execution paths, and provides clearer status messages. Also updated compute_stats.exe binary.

--- run_all.bat
+++ run_all.bat
@@ -1,18 +1,21 @@
 @echo off
-echo =======================================
+setlocal
+
+set start=%time%
+echo ========================================
+echo ===   Iniciando Pipeline Fortran+Java ===
+echo ========================================
+echo Inicio: %start%
+
+echo ========================================
 echo ===  Compilando y ejecutando Fortran ===
-echo =======================================
+echo ========================================
 
-:: Ir a la carpeta fortran
 cd fortran
-
-:: Compilar el programa de estadísticas
+:: borrar el exe viejo si existe (evita "Acceso denegado")
+if exist ..\compute_stats.exe del ..\compute_stats.exe
 gfortran compute_stats.f90 -o ..\compute_stats.exe
-
-:: Ejecutar el binario (input.csv -> stats.csv + errors.log)
 ..\compute_stats.exe input.csv stats.csv errors.log
-
-:: Volver a la carpeta raíz
 cd ..
 
 echo ===================================
@@ -20,13 +23,22 @@ echo ===  Ejecutando Java con Maven  ===
 echo ===================================
 
 cd java\java
-
-:: Ejecutar la clase principal de Java con Maven
-mvn exec:java -Dexec.mainClass="main.Main" -Dexec.args="fortran\out\stats.csv java\java\in\rules.json"
-
+mvn exec:java -Dexec.mainClass="main.Main" -Dexec.args="..\..\fortran\stats.csv in\rules.json"
 cd ..\..
 
+set end=%time%
+echo Fin: %end%
+
+:: Calcular duración en segundos usando PowerShell
+:: Calcular duración en formato mm:ss usando PowerShell
+for /f "tokens=* usebackq" %%a in (`powershell -NoProfile -Command ^
+    "$d=(New-TimeSpan -Start '%start%' -End '%end%'); '{0:D2}:{1:D2}' -f $d.Minutes,$d.Seconds"`) do set duration=%%a
+
 echo ================================
-echo ===  Pipeline Finalizado 😀  ===
+echo ===  Pipeline Finalizado     ===
+echo ===  Tiempo total: %duration% min:seg
 echo ================================
+
+
 pause
+endlocal
